e6fca01c950fd8f2c547a78fb8d8bb485bd2a18d
lkruitwagen <lucas.kruitwagen@gmail.com>
2020-06-21 18:54:51 +0000

    lastmile

---
+++ ffsc/lastmile/extract_firstmile.py
@@ -0,0 +1,8 @@
+
+
+def firstmile(asset):
+    """
+    asset one of [coalmines, field, wells];
+    match coalmines to closest port, railhead, or city
+    match fields, wells to closest port, pipeline or city
+    """
---
+++ ffsc/lastmile/powerstn_lastmile.py
@@ -0,0 +1,7 @@
+
+
+def powerstn_lastmile(carrier):
+    """
+    match coal powerstns to railhead, port, or city
+    match gas or oil powerstns to pipeline, port, or city
+    """
